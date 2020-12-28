bb880e166c2dc79569fbeb438467f006b3454a0b
Huanjie Sheng <drhjsheng@gmail.com>
2020-12-28 11:34:18 -0800

    test plugin scripts

---
+++ GTmetrix Extension/popup.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      // var f = d.createElement('form');
+      // f.action = 'http://gtmetrix.com/analyze.html?bm';
+      // f.method = 'post';
+      // var i = d.createElement('input');
+      // i.type = 'hidden';
+      // i.name = 'url';
+      // i.value = tab.url;
+      // f.appendChild(i);
+      // d.body.appendChild(f);
+      // f.submit();
+
+      var newURL = "http://stackoverflow.com/";
+      chrome.tabs.create({ url: newURL });
+
+    });
+  }, false);
+}, false);
---
+++ StockWatcher/popup.js
@@ -0,0 +1,24 @@
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs.id,{
+        code: 'document.body.style.backgroundColor = "' + color + '";'
+        //If you had something somewhat more complex you can use an IIFE:
+        //code: '(function (){return document.body.innerText;})();'
+        //If your code was complex, you should store it in a
+        // separate .js file, which you inject with the file: property.
+    });
+  });
+};
+
+function setTheColor() {
+  document.body.style.backgroundColor = "' + color + '";
+  console.log("test me");
+}
